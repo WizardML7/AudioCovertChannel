c54e0dc7797de803d868820440ddd825a46d530a
WizardML7 <dml3483@g.rit.edu>
2024-02-21 15:19:35 -0500

    Added a great comment

--- audio_cc.py
+++ audio_cc.py
@@ -2,8 +2,23 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def plot_audio_spectrum(wav_file_path, time_range=None, frequency_range=None, nfft=1024, cmap='viridis'):
+    """
+    Plots the spectrogram of an audio file specified by 'wav_file_path'.
+    
+    This function opens a WAV file, reads its frames, and then converts these frames into a NumPy array to represent the audio signal. 
+    It uses the matplotlib library to plot a spectrogram of the audio signal, which is a visual representation of the spectrum of 
+    frequencies in the audio signal as they vary with time. The spectrogram is plotted with the time on the x-axis and frequency on the y-axis.
+    
+    Parameters:
+    - wav_file_path: Path to the WAV file to be analyzed.
+    - time_range: Optional tuple specifying the time range (start, end) in seconds to plot. If None, plots the entire duration.
+    - frequency_range: Optional tuple specifying the frequency range (low, high) in Hz to plot. If None, plots the full frequency range captured.
+    - nfft: Number of FFT points; higher values provide finer frequency resolution. Default is 1024.
+    - cmap: Colormap for the spectrogram. Default is 'viridis'.
+    
+    The function displays the spectrogram using the specified colormap and adjusts the plot limits based on the provided time and frequency ranges, if any.
+    """
     # Open the WAV file
     with wave.open(wav_file_path, 'rb') as wav_file:
         # Get the audio sample rate and number of frames
