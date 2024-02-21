ccd7f8460598ce58e590ef5f4f7d6da055542104
Domenic Lo Iacono <89399375+WizardML7@users.noreply.github.com>
2024-02-21 15:05:27 -0500

    Create audio_cc.py

---
+++ audio_cc.py
@@ -0,0 +1,29 @@
+def plot_audio_spectrum(wav_file_path, time_range=None, frequency_range=None, nfft=1024, cmap='viridis'):
+    # Open the WAV file
+    with wave.open(wav_file_path, 'rb') as wav_file:
+        # Get the audio sample rate and number of frames
+        sample_rate = wav_file.getframerate()
+        num_frames = wav_file.getnframes()
+
+        # Read all frames
+        frames = wav_file.readframes(num_frames)
+
+    # Convert frames to a NumPy array of samples
+    samples = np.frombuffer(frames, dtype=np.uint8)  # Use uint8 for 8 bits per sample
+
+    # Plot the spectrum of the audio file with adjusted parameters
+    plt.figure(figsize=(10, 4))
+    plt.specgram(samples, NFFT=nfft, Fs=sample_rate, cmap=cmap)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Spectrogram of the Audio File')
+
+    # Set x-axis limits based on the specified time range
+    if time_range:
+        plt.xlim(time_range)
+
+    # Set y-axis limits based on the specified frequency range
+    if frequency_range:
+        plt.ylim(frequency_range)
+
+    plt.show()
